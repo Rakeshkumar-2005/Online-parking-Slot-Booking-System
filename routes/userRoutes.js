caa9d781cd8af3e3890606a43f950dfff1535337
Rakesh Kumar <rakeshkumar763300@gmail.com>
2026-07-24 23:50:01 +0530

    Added user login API

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
+// Register
 router.post("/register", async (req, res) => {
     try {
 
         const { name, email, password } = req.body;
 
-        // Check existing user
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
@@ -26,6 +26,39 @@ router.post("/register", async (req, res) => {
 
         res.status(201).json({
             message: "User Registered Successfully",
+            user
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+
+// Login
+router.post("/login", async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "User not found"
+            });
+        }
+
+        if (user.password !== password) {
+            return res.status(400).json({
+                message: "Invalid password"
+            });
+        }
+
+        res.status(200).json({
+            message: "Login Successful",
             user
         });
 
